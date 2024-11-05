3e010f6d1e0851e9c45f081339aabaa390577479
Yipmong Said <Syipmong5@gmail.com>
2024-11-05 14:55:13 +0100

    Refactor font imports and update Lusitana configuration for improved performance

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,5 +3,4 @@ import { Lusitana } from 'next/font/google';
 import { Tiny5 } from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']})
-export const lusitana = Lusitana({ weight: '400' })
-export const tiny5 = Tiny5({weight: '400', style: 'normal'})
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' })

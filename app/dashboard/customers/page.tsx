984202388908dd836470d8c7a5eba54ef09caf18
Yipmong Said <Syipmong5@gmail.com>
2024-11-05 14:45:35 +0100

    Add dashboard layout and pages for customers and invoices

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+    return (
+        <div className="page">
+            <div className="page__header">
+                <h1 className="page__title">Customers page</h1>
+            </div>
+        </div>
+        );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+export default function Page(){
+    return (
+        <div className="page">
+            <div className="page__header">
+                <h1 className="page__title">Invoices</h1>
+
+            </div>
+        </div>
+    );
+}

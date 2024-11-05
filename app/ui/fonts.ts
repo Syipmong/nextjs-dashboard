65b39629535bdd75a46dd61279d2abb19ace58e8
Yipmong Said <Syipmong5@gmail.com>
2024-11-05 11:57:20 +0100

    Add custom font imports and apply styles to layout and page components

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Tiny5 } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']})
+export const lusitana = Lusitana({ weight: '400' })
+export const tiny5 = Tiny5({weight: '400', style: 'normal'})

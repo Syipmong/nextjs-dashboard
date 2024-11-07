7ff6919855b7f62b393f7b63519c6bf600610b0b
Yipmong Said <Syipmong5@gmail.com>
2024-11-07 10:10:26 +0100

    Add form validation schema for invoice creation using Zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,23 @@
 'use server'
 
+import {z} from 'zod'
+import { CreateInvoice } from '../ui/invoices/buttons';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['paid','pending']),
+    date: z.string()
+})
+
+const CreateInvoice = FormSchema.omit(
+    {
+        id: true,
+        date: true
+    }
+)
+
 export async function createInvoice(formData: FormData) {
     const rawFormData = {
         customerId: formData.get('customerId'),

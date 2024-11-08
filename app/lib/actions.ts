7437d4aba14820f780a4a12081997df043390c7f
Yipmong Said <Syipmong5@gmail.com>
2024-11-08 07:46:50 +0100

    Add ESLint configuration and implement form validation for invoice creation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,13 +4,17 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { error } from 'console'
+
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['paid','pending']),
+    customerId: z.string({
+        invalid_type_error: 'Select a valid customer',
+    }),
+    amount: z.coerce.number().gt(0, 'Amount must be greater than 0'),
+    status: z.enum(['paid','pending'],{
+        invalid_type_error: 'Select an invoice status'
+    }),
     date: z.string()
 })
 
@@ -25,6 +29,15 @@ const UpdateInvoice = FormSchema.omit({
     id: true,
     date: true
 })
+
+export type State = {
+    errors?: {
+        customerId? : string[],
+        amount?: string[],
+        status?: string[]
+    }
+    message?: string | null
+}
 
 export async function updateInvoice(id: string, formData: FormData){
     const { customerId, amount, status} = UpdateInvoice.parse({
@@ -49,12 +62,21 @@ export async function updateInvoice(id: string, formData: FormData){
     redirect('/dashboard/invoices')
 }
 
-export async function createInvoice(formData: FormData){
-        const {customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData){
+        const validateFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
         });
+
+
+        if(!validateFields.success){
+            return{
+                errors: validateFields.error.flatten().fieldErrors,
+                message: 'Missing Fields: Failed to create invoice'
+            }
+        }
+        const { customerId, amount, status} = validateFields.data;
         const amountInCents = amount * 100;
         const date = new Date().toISOString().split('T')[0];
 
